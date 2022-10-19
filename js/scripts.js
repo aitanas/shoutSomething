732a2ee7096a46bebefde587232b533bfa0d40d3
Aitana Shough <aeshough@gmail.com>
2022-10-19 11:14:43 -0700

    add function to increase font size on form submission
    Co-authored-by: Anton Ch <anton3ch@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 window.onload = function() {
   let form = document.querySelector("form");
+  let fontSize = "14px";
+  document.querySelector("p").style.fontSize = fontSize;
+  // onSubmit function
   form.onsubmit = function(event) {
     event.preventDefault();
     // get value for form input
@@ -11,6 +14,23 @@ window.onload = function() {
     document.querySelector("span#shoutSomething").innerText = shoutSomething; 
     document.querySelector("div#shout").removeAttribute("class");
     
+    let fontSize = parseInt(document.querySelector("p").style.fontSize);
+    console.log(fontSize);
+    let increaseFont = parseInt(fontSize) + 1;
+    console.log(increaseFont);
+    let updateFontSize = increaseFont + 'px';
+    console.log(updateFontSize);
+    document.querySelector("p").style.fontSize = updateFontSize;
+    
+    // fontSize++;
      // window.alert(typeSomething.toUpperCase());
   }
 }
+
+// function increaseSize() {
+//   form.onclick = fontSize + 5;
+  
+// }
+
+//let newFontSize = parseInt(fontSize) + "px";
+//document.querySelector("p").style.fontSize = newFontSize;
